9c3d9a5e91917aabd07e986317277a57f2ca5593
god-damn <150293273+god-damn@users.noreply.github.com>
2024-07-31 18:43:37 +0300

    Delete Travel Landing Page directory

--- Travel Landing Page/js/main.js
+++
@@ -1,8 +0,0 @@
-const burgerMenu = document.querySelector(".burger-btn");
-const header = document.querySelector(".header__top");
-
-burgerMenu.addEventListener("click", () => {
-  burgerMenu.classList.toggle("burger-btn--active");
-  document.body.classList.toggle("no-scroll");
-  header.classList.toggle("header__mobile");
-});
